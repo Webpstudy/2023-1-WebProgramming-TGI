e6f4af9afe30cf2d956ff62dd2ccb702bef57813
2dydgh <chrisyongho@gmail.com>
2023-05-25 05:47:00 +0900

    feat: 이용호 캘린더 기능 추가

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -3,8 +3,9 @@ import { formatDate } from "@fullcalendar/core";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
 import { INITIAL_EVENTS, createEventId2, initialIds } from "./event-utils";
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
+import { v4 as uuidv4 } from "uuid";
 import "./calendar.css";
 
 function Calendar() {
@@ -16,6 +17,20 @@ function Calendar() {
     const storedEvents = JSON.parse(localStorage.getItem("events"));
     if (storedEvents) {
       setEvents(storedEvents);
+    }
+  }, []);
+
+  useEffect(() => {
+    const storedExternalEvents = JSON.parse(
+      localStorage.getItem("externalEvents")
+    );
+    if (storedExternalEvents) {
+      setState((state) => {
+        return {
+          ...state,
+          externalEvents: storedExternalEvents,
+        };
+      });
     }
   }, []);
 
@@ -40,7 +55,7 @@ function Calendar() {
   }
 
   const handleDateSelect = (selectInfo) => {
-    let title = prompt("새로운 이벤트 이름을 입력해주세요");
+    let title = prompt("새로운 일정을 추가해주세요");
     let calendarApi = selectInfo.view.calendar;
 
     calendarApi.unselect(); // clear date selection
@@ -52,18 +67,17 @@ function Calendar() {
         start: selectInfo.startStr,
         end: selectInfo.endStr,
         allDay: selectInfo.allDay,
-        backgroundColor: "green",
-        borderColor: "green",
+        backgroundColor: "#c9c8cd",
+        borderColor: "#c9c8cd",
       });
-
       const newEvent = {
-        id: createEventId2(),
+        id: uuidv4(),
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
         allDay: selectInfo.allDay,
-        backgroundColor: "green",
-        borderColor: "green",
+        backgroundColor: "#c9c8cd",
+        borderColor: "#c9c8cd",
       };
 
       const updatedEvents = [...events, newEvent];
@@ -71,6 +85,15 @@ function Calendar() {
       localStorage.setItem("events", JSON.stringify(updatedEvents));
     }
   };
+
+  function checkIfEventIncluded(eventId) {
+    //초기이벤트에 포함되어있는지 확인하는 함수
+    return initialIds
+      .map((array) => {
+        return array.toString();
+      })
+      .includes(eventId);
+  }
 
   const handleEventClick = (clickInfo) => {
     var eventId = clickInfo.event.id;
@@ -89,23 +112,37 @@ function Calendar() {
     }
   };
 
-  function checkIfEventIncluded(eventId) {
-    //초기이벤트에 포함되어있는지 확인하는 함수
-    return initialIds
-      .map((array) => {
-        return array.toString();
-      })
-      .includes(eventId);
-  }
-
+   // 외부 이벤트 목록에서 삭제
+  const handleExternalEventRemove = (eventId) => {
+    setState((prevState) => {
+      const updatedExternalEvents = prevState.externalEvents.filter(
+        (event) => event.id !== eventId
+      );
+      localStorage.setItem(
+        "externalEvents",
+        JSON.stringify(updatedExternalEvents)
+      );
+      return {
+        ...prevState,
+        externalEvents: updatedExternalEvents,
+      };
+    });
+  };
+  
+   // 이벤트에서 삭제
   function handleEventRemove(eventInfo) {
-    const updatedEvents = events.filter(
-      (event) => event.id !== eventInfo.event.id
-    );
-    setEvents(updatedEvents);
-    localStorage.setItem("events", JSON.stringify(updatedEvents));
+    const isExternalEvent = checkIfEventIncluded(eventInfo.event.id); 
+  
+    if (isExternalEvent) {
+      handleExternalEventRemove(eventInfo.event.id); // 외부 이벤트 삭제 함수 호출
+    } else {
+      const updatedEvents = events.filter(
+        (event) => event.id !== eventInfo.event.id
+      );
+      setEvents(updatedEvents);
+      localStorage.setItem("events", JSON.stringify(updatedEvents));
+    }
   }
-
   const handleEvents = (events) => {
     setCurrentEvents(events);
   };
@@ -119,37 +156,167 @@ function Calendar() {
     );
   };
 
+  // initial state
+  const [state, setState] = useState({
+    weekendsVisible: true,
+    externalEvents: [
+      { title: "이벤트1", color: "#f1a8bc", id: 34432, custom: "부가설명" },
+      { title: "이벤트2", color: "#eee58a", id: 323232 },
+      { title: "이벤트3", color: "#c4ecb0", id: 1111 },
+      { title: "이벤트4", color: "#addbd8", id: 432432 },
+    ],
+  });
+
+  // load external events
+  useEffect(() => {
+    let draggableEl = document.getElementById("external-events");
+    let draggable = new Draggable(draggableEl, {
+      itemSelector: ".fc-event",
+      eventData: function (eventEl) {
+        let id = eventEl.dataset.id;
+        let title = eventEl.getAttribute("title");
+        let color = eventEl.dataset.color;
+        let custom = eventEl.dataset.custom;
+
+        return {
+          id: id,
+          title: title,
+          color: color,
+          custom: custom,
+          create: true,
+        };
+      },
+    });
+    return () => {
+      draggable.destroy(); // 컴포넌트 언마운트 시 Draggable 인스턴스를 정리
+    };
+  }, []);
+
+  // add external events
+  const addEvent = () => {
+    let title = prompt("추가된 이벤트의 제목을 입력하세요");
+    let custom = prompt("추가된 이벤트의 부가설명을 입력하세요");
+    if (title) {
+      let newEvent = {
+        id: uuidv4(), // 고유한 UUID 생성
+        title: title,
+        color: getColorByIndex(state.externalEvents.length), // 외부 이벤트 목록의 길이에 따라 색상 할당
+        start: "2020-12-31",
+        end: "2020-12-31",
+        custom: custom,
+      };
+      setState((prevState) => {
+        const updatedExternalEvents = [...prevState.externalEvents, newEvent];
+        localStorage.setItem(
+          "externalEvents",
+          JSON.stringify(updatedExternalEvents)
+        );
+        return {
+          ...prevState,
+          externalEvents: updatedExternalEvents,
+        };
+      });
+
+       // FullCalendar에도 이벤트 추가
+    const calendarApi = calendarRef.current.getApi(); // FullCalendar의 API 객체 가져오기
+    calendarApi.addEvent(newEvent)
+    }
+  };
+
+  const handleEventAdd = (eventInfo) => {
+    // 이벤트 추가 로직
+    const newEvent = {
+      id: eventInfo.event.id,
+      title: eventInfo.event.title,
+      start: eventInfo.event.startStr,
+      end: eventInfo.event.endStr || null,
+      allDay: eventInfo.event.allDay,
+      backgroundColor: eventInfo.event.backgroundColor,
+      borderColor: eventInfo.event.borderColor,
+    };
+  
+    // events 배열에 추가된 이벤트 저장
+    const updatedEvents = [...events, newEvent];
+    setEvents(updatedEvents);
+    localStorage.setItem("events", JSON.stringify(updatedEvents));
+  };
+  
+  // 외부 이벤트 목록의 인덱스에 따라 색상을 할당하는 함수
+  const getColorByIndex = (index) => {
+    const colors = ["#f1a8bc", "#eee58a", "#c4ecb0", "#addbd8", "#d6b0eb"];
+    const colorIndex = index % colors.length;
+    return colors[colorIndex];
+  };
+
   const mergedEvents = [...INITIAL_EVENTS, ...events];
 
   return (
     <div className="calendar-app">
       <div className="calendar-app-main">
-        <FullCalendar
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek,timeGridDay",
-          }}
-          initialView="dayGridMonth"
-          editable={true}
-          selectable={true}
-          selectMirror={true}
-          dayMaxEvents={true}
-          weekends={weekendsVisible}
-          select={handleDateSelect}
-          eventDrop={handleEventDrop}
-          eventContent={renderEventContent} // custom render function
-          eventClick={handleEventClick}
-          eventsSet={handleEvents} // called after events are initialized/added/changed/removed
-          /* you can update a remote database when these fire:
+        <div id="external-events">
+          <div style={{ margin: " 0 0 20px" }}>
+            <input
+              type="submit"
+              name="name"
+              onClick={addEvent}
+              value="이벤트 추가"
+            />
+          </div>
+          {state.externalEvents.map((event) => (
+            <div
+              className="fc-event fc-h-event mb-1 fc-daygrid-event fc-daygrid-block-event p-2"
+              title={event.title}
+              data-id={event.id}
+              data-color={event.color}
+              data-custom={event.custom}
+              key={event.id}
+              style={{
+                backgroundColor: event.color,
+                borderColor: event.color,
+                cursor: "pointer",
+              }}
+            >
+              <div className="fc-event-main">
+                <div>
+                  <strong>{event.title}</strong>
+                </div>
+                {event.custom}
+              </div>
+              <button onClick={() => handleExternalEventRemove(event.id)}>
+                삭제
+              </button>
+            </div>
+          ))}
+        </div>
+        <div id="calendar-main">
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            weekends={weekendsVisible}
+            select={handleDateSelect}
+            eventDrop={handleEventDrop}
+            eventContent={renderEventContent} // custom render function
+            eventClick={handleEventClick}
+            eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+            droppable={true}
+            /* you can update a remote database when these fire:
           eventAdd={function(){}}
           eventChange={function(){}}
           eventRemove={function(){}}
           */
-          events={mergedEvents}
-          eventRemove={handleEventRemove}
-        />
+            events={mergedEvents}
+            eventRemove={handleEventRemove}
+          />
+        </div>
       </div>
     </div>
   );
